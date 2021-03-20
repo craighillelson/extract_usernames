9ab314ce10cbac86b0bfa26883b2b6e261e87965
Craig Hillelson <craighillelson@gmail.com>
2021-03-19 22:53:23 -0400

    update extract_usernames.ps1
    simplified

--- extract_usernames.ps1
+++ extract_usernames.ps1
@@ -1,21 +1,9 @@
-# import csv
-$users = Import-Csv -Path "users.csv"
+$updated_usernames = @()
 
-# create empty array
-$updated_users = @()
-
-# loop through $users array and remove "domain\" from each username
-foreach ($user in $users) {
-    $user = $user.user
-    $user = $user.split("\")
-    $user = $user[1]
-    $updated_users += $user
+Import-Csv -Path 'users.csv' | ForEach-Object {
+    $updated_username = $_.users.split("\")[1]
+    $updated_usernames += $updated_username
 }
 
-# output results
-Write-Host("`nupdated usernames")
-$updated_users
-
-# output results to csv
-$updated_users | Out-File updated_usernames.csv
-Write-Host("`n'updated_usernames.csv' was exported successfuly")
+$updated_usernames | Out-File updated_usernames.csv
+Write-Output("`n'updated_usernames.csv' exported successfully")
